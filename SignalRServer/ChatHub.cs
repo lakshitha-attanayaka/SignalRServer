659376c3653de682d8c22148d38236bac6c83e2f
Lakshitha Attanayaka <lakshitha.attanayaka@gmail.com>
2022-07-16 23:33:39 +0530

    Add project files.

---
+++ SignalRServer/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRServer
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", message);
+        }
+    }
+}
